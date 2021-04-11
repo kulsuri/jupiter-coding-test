e9764f21f1d9c182423beb37275b7641ac657798
Kulraj Suri <kulraj.suri@gmail.com>
2021-04-11 02:06:18 +0100

    flask web app working

--- app.py
+++ app.py
@@ -4,15 +4,36 @@ from transform import transformESGData
 from create_pandas_df import createDF
 from visualisation_engine import visualisationEngine
 
-import matplotlib.pyplot as plt
-import pprint
+from flask import Flask, render_template, request
+import requests
 
-ticker_symbol = 'jpm'
+app = Flask(__name__)
 
-data1 = bot(ticker_symbol).initializeScrapeProcess()
-data2 = transformESGData(data1).initializeESGTranformProcess()
-data3 = createDF(ticker_symbol, data2).initializeDF()
-data4 = visualisationEngine(data3).initializePlot()
+@app.route('/')
+def index():
+    return render_template("index.html")
 
-# data4
-# plt.show()
+@app.route('/', methods=['POST'])
+def my_form_post():
+    ticker = request.form['text']
+    if ticker != "":
+        df = web_scraper(ticker)
+        return html_table(df)
+    else:
+        return render_template("index.html")
+
+@app.route('/', methods=("POST", "GET"))
+def html_table(df):
+    return render_template('simple.html',  tables=[df.to_html(classes='data')], titles=df.columns.values)
+
+#ticker_symbol = 'jpm'
+
+def web_scraper(ticker_symbol):
+    data1 = bot(ticker_symbol).initializeScrapeProcess()
+    data2 = transformESGData(data1).initializeESGTranformProcess()
+    data3 = createDF(ticker_symbol, data2).initializeDF()
+    return data3
+# data4 = visualisationEngine(data3).initializePlot()
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1')
