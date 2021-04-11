1b06fdb20e73399e8fd69e22f75c3d0404d039a5
Kulraj Suri <kulraj.suri@gmail.com>
2021-04-11 01:15:53 +0100

    visualisation engine added

--- app.py
+++ app.py
@@ -2,13 +2,17 @@ from get_esg_score import bot
 from get_stock_price import historicalPrices
 from transform import transformESGData
 from create_pandas_df import createDF
+from visualisation_engine import visualisationEngine
+
+import matplotlib.pyplot as plt
 import pprint
 
-ticker_symbol = 'goog'
+ticker_symbol = 'jpm'
 
 data1 = bot(ticker_symbol).initializeScrapeProcess()
 data2 = transformESGData(data1).initializeESGTranformProcess()
 data3 = createDF(ticker_symbol, data2).initializeDF()
+data4 = visualisationEngine(data3).initializePlot()
 
-
-print(data3)
+# data4
+# plt.show()
--- visualisation_engine.py
+++ visualisation_engine.py
@@ -1,11 +1,32 @@
-esg_score_to_num_map = {
-    'AAA' : 9,
-    'AA' : 8,
-    'A' : 7,
-    'BBB' : 6,
-    'BB' : 5,
-    'B' : 4,
-    'CCC' : 3,
-    'CC' : 2,
-    'C' : 1
-}
+import matplotlib.pyplot as plt
+
+class visualisationEngine:
+    def __init__(self, esg_price_df):
+        self.esg_price_df = esg_price_df
+
+    def assign_num_val_to_esg_scores(self):
+        esg_score_to_num_map = {
+            'AAA' : 9,
+            'AA' : 8,
+            'A' : 7,
+            'BBB' : 6,
+            'BB' : 5,
+            'B' : 4,
+            'CCC' : 3,
+            'CC' : 2,
+            'C' : 1
+        }
+
+        plotable_df = self.esg_price_df
+        plotable_df['ESG Value'] = plotable_df['ESG Score'].map(esg_score_to_num_map)
+        return plotable_df
+
+    def plot_df(self, plotable_df):
+        plotable_df.plot(y='Adj Close')
+        bx = plotable_df['ESG Value'].plot(secondary_y=True, color='k', marker='o')
+        return bx #plt.show()
+
+    def initializePlot(self):
+        df = self.assign_num_val_to_esg_scores()
+        viz = self.plot_df(df)
+        return plt.show()
